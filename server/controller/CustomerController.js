bf907664d535bbdd7ce82ee9503a337497937860
hsinoj1996 <hsinojjonish@gmail.com>
2020-07-31 12:10:02 +0545

    validation completed by using isEmpty and NPM validator

--- server/controller/CustomerController.js
+++ server/controller/CustomerController.js
@@ -6,44 +6,60 @@ const bcrypt = require("bcryptjs")
 const keys = require("../configuration/keys")
 const jwt = require("jsonwebtoken");
 
+const customerValidation = require("../validation/registerValidation")
+
 //POST Method
 module.exports.customerRegister = (req,res) => {
     
-    const register = new User({
-        Firstname: req.body.Firstname,
-        Lastname: req.body.Lastname,
-        Password: req.body.Password,
-        Confirm_Password: req.body.Confirm_Password,
-        Gender: req.body.Gender,
-        Email: req.body.Email
-    });
-    bcrypt.genSalt(10, (error, salt) =>{
-        bcrypt.hash(register.Password, salt, (err, hash) => {
-            if(err){
-                res.json({
-                    err
-                })
-            }else{
-                register.Password = hash;
-                register.save()
-                .then((savedCustomer)=>{
-                    if(savedCustomer){
-                        res.status(201).json({
-                            success: true,
-                            message: "Account Successfully Created!!!",
-                            savedCustomer
+    const {errors, isValid} = customerValidation(req.body);
+
+    if(!isValid){
+        return res.json(errors)
+    }
+    //checking if email is already registered or not
+    User.findOne({Email: req.body.Email}).then((isCheck) =>{
+        if(isCheck){
+            res.json({
+                message:"already registered"
+            })
+        }else{
+            const register = new User({
+                Firstname: req.body.Firstname,
+                Lastname: req.body.Lastname,
+                Password: req.body.Password,
+                Confirm_Password: req.body.Confirm_Password,
+                Gender: req.body.Gender,
+                Email: req.body.Email
+            });
+            bcrypt.genSalt(10, (error, salt) =>{
+                bcrypt.hash(register.Password, salt, (err, hash) => {
+                    if(err){
+                        res.json({
+                            err
                         })
                     }else{
-                        res.status(406).json({
-                            message:"Cannot Created"
+                        register.Password = hash;
+                        register.save()
+                        .then((savedCustomer)=>{
+                            if(savedCustomer){
+                                res.status(201).json({
+                                    success: true,
+                                    message: "Account Successfully Created!!!",
+                                    savedCustomer
+                                })
+                            }else{
+                                res.status(406).json({
+                                    message:"Cannot Created"
+                                })
+                            }
+                        }).catch((error)=>{
+                            message:"internal error",
+                            error
                         })
                     }
-                }).catch((error)=>{
-                    message:"internal error",
-                    error
                 })
-            }
-        })
+            })
+        }
     })
 }
 
@@ -91,7 +107,7 @@ module.exports.customerEdit = (req,res) =>{
     })
 }
 
-//Login Method
+//LOGIN Method
 module.exports.customerLogin = (req,res) =>{
     const Email = req.body.Email;
     const Password = req.body.Password
@@ -100,10 +116,12 @@ module.exports.customerLogin = (req,res) =>{
         bcrypt.compare(Password, isLoggedIn.Password).then((isMatched) =>{
             if(isMatched){
 
+                //loading data into payload
                 const payload = {
                     id:isLoggedIn.id,
                     Email: isLoggedIn.Email
                 }
+                //signing in the web with secret keys,datas and expiretime
                 jwt.sign(
                     payload,
                     keys.secretOrKey,
---
+++ server/validation/registerValidation.js
@@ -0,0 +1,46 @@
+const Validator = require ("validator");
+const isEmpty = require("./isEmpty")
+
+const errorValid = customerData =>{
+    let errors = {};
+
+    customerData.Firstname = !isEmpty(customerData.Firstname) ? customerData.Firstname:"";
+    customerData.Lastname = !isEmpty(customerData.Lastname) ? customerData.Lastname:"";
+    customerData.Password = !isEmpty(customerData.Password) ? customerData.Password:"";
+    customerData.Confirm_Password = !isEmpty(customerData.Confirm_Password) ? customerData.Confirm_Password:"";
+    customerData.Gender = !isEmpty(customerData.Gender) ? customerData.Gender:"";
+    customerData.Email = !isEmpty(customerData.Email) ? customerData.Email:"";
+
+    if(Validator.isEmpty(customerData.Firstname)){
+        errors.Firstname = "Firstname is required"
+    }
+    if(Validator.isEmpty(customerData.Lastname )){
+        errors.Lastname = "Lastname is required";
+    }
+
+    if(Validator.isEmpty(customerData.Password )){
+        errors.Password = "Password is required";
+    }
+
+    if(Validator.isEmpty(customerData.Confirm_Password )){
+        errors.Confirm_Password = "Confirm_Password is required";
+    }
+    if(customerData.Password != customerData.Confirm_Password){
+        errors.message = "Password donot matches"
+    }
+
+    if(Validator.isEmpty(customerData.Gender )){
+        errors.Gender = "Gender is required";
+    }
+
+    if(Validator.isEmpty(customerData.Email )){
+        errors.Email = "Email is required";
+    }
+
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+module.exports = errorValid
